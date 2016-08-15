01367d8ce696d9fe104edaa8448a73ba188a8161
lihuaixin <1072931117@qq.com>
2016-08-15 16:45:08 +0800

    添加编辑器浏览测试

--- src/test/java/com/huotu/hotcms/widget/logoWall/TestWidgetInfo.java
+++ src/test/java/com/huotu/hotcms/widget/logoWall/TestWidgetInfo.java
@@ -16,7 +16,7 @@ import com.huotu.widget.test.WidgetTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -36,16 +36,10 @@ public class TestWidgetInfo extends WidgetTest {
 
     @Override
     protected void editorWork(Widget widget, WebElement editor, Supplier<Map<String, Object>> currentWidgetProperties) {
-        try{
-            currentWidgetProperties.get();
-        }catch (IllegalStateException ignored){
-            assertThat(0).as("save没有属性值返回异常").isEqualTo(0);
-        }
-
         List<WebElement> input = editor.findElements(By.tagName("input"));
         assertThat(input.size()).isNotEqualTo(0);
         try{
-            currentWidgetProperties.get();
+            Map map = currentWidgetProperties.get();
         }catch (IllegalStateException ignored){
             assertThat(0).as("save没有属性值返回异常").isEqualTo(0);
         }
@@ -53,19 +47,16 @@ public class TestWidgetInfo extends WidgetTest {
     }
 
     @Override
-    protected void browseWork(Widget widget, WidgetStyle style, Function<ComponentProperties, WebElement> uiChanger) {
-
-        ComponentProperties properties = new ComponentProperties();
-        List<String> list = new ArrayList<>();
-
-        list.add("http://placehold.it/106x82?text=logo1");
-        list.add("http://placehold.it/106x82?text=logo2");
-        properties.put(WidgetInfo.VALID_LOGO_LINK_LIST,list);
-
+    protected void browseWork(Widget widget, WidgetStyle style, Function<ComponentProperties, WebElement> uiChanger) throws IOException {
+        ComponentProperties properties = widget.defaultProperties(resourceService);
         WebElement webElement = uiChanger.apply(properties);
-
         List<WebElement> img = webElement.findElements(By.tagName("img"));
-        assertThat(img.size()).isEqualTo(2);
+        assertThat(img.size()).isEqualTo(4);
+
+    }
+
+    @Override
+    protected void editorBrowseWork(Widget widget, Function<ComponentProperties, WebElement> uiChanger) throws IOException {
 
     }
 
